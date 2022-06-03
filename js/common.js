d6503d4639e33daf9fc63430612c0b4bbc0757f9
hwang1588 <75598328+hwang1588@users.noreply.github.com>
2022-06-03 17:21:22 +0900

    버튼 업데이트

--- js/common.js
+++ js/common.js
@@ -28,38 +28,67 @@ window.setTimeout(function () {
 
 
 
-  $(".header-container").hover(function () { //"test_enter"요소에 '마우스가진입'하면
-    $(this).addClass("bg-white").removeClass("bg-transparent");
-    $(".header-logo").addClass("header-logo-black").removeClass("header-logo-white");
-    $(".nav-link").addClass("text-black").removeClass("text-white");
-    $(".header_sns_black").addClass("d-block").removeClass("d-none");
-    $(".header_sns_white").addClass("d-none").removeClass("d-block");
+$(".header-container").hover(function () { //"test_enter"요소에 '마우스가진입'하면
+  $(this).addClass("bg-white").removeClass("bg-transparent");
+  $(".header-logo").addClass("header-logo-black").removeClass("header-logo-white");
+  $(".nav-link").addClass("text-black").removeClass("text-white");
+  $(".header_sns_black").addClass("d-block").removeClass("d-none");
+  $(".header_sns_white").addClass("d-none").removeClass("d-block");
 
-  },function(){
-    $(this).addClass("bg-transparent").removeClass("bg-white");
-    $(".header-logo").addClass("header-logo-white").removeClass("header-logo-black");
-    $(".nav-link").addClass("text-white").removeClass("text-black");
-    $(".header_sns_white").addClass("d-block").removeClass("d-none");
-    $(".header_sns_black").addClass("d-none").removeClass("d-block");
-  });
+}, function () {
+  $(this).addClass("bg-transparent").removeClass("bg-white");
+  $(".header-logo").addClass("header-logo-white").removeClass("header-logo-black");
+  $(".nav-link").addClass("text-white").removeClass("text-black");
+  $(".header_sns_white").addClass("d-block").removeClass("d-none");
+  $(".header_sns_black").addClass("d-none").removeClass("d-block");
+});
 
-  $(".avatar1").hover(function () { //"test_enter"요소에 '마우스가진입'하면
-    $(".bubble1").addClass("visible").removeClass("invisible");
-    $(".bubble2, .bubble3, .bubble4, .bubble5").addClass("invisible").removeClass("visible");
+$(".avatar1").hover(function () { //"test_enter"요소에 '마우스가진입'하면
+  $(".bubble1").addClass("visible").removeClass("invisible");
+  $(".bubble2, .bubble3, .bubble4, .bubble5").addClass("invisible").removeClass("visible");
+});
+$(".avatar2").hover(function () { //"test_enter"요소에 '마우스가진입'하면
+  $(".bubble2").addClass("visible").removeClass("invisible");
+  $(".bubble1, .bubble3, .bubble4, .bubble5").addClass("invisible").removeClass("visible");
+});
+$(".avatar3").hover(function () { //"test_enter"요소에 '마우스가진입'하면
+  $(".bubble3").addClass("visible").removeClass("invisible");
+  $(".bubble1, .bubble2, .bubble4, .bubble5").addClass("invisible").removeClass("visible");
+});
+$(".avatar4").hover(function () { //"test_enter"요소에 '마우스가진입'하면
+  $(".bubble4").addClass("visible").removeClass("invisible");
+  $(".bubble1, .bubble2, .bubble3, .bubble5").addClass("invisible").removeClass("visible");
+});
+$(".avatar5").hover(function () { //"test_enter"요소에 '마우스가진입'하면
+  $(".bubble5").addClass("visible").removeClass("invisible");
+  $(".bubble1, .bubble2, .bubble3, .bubble4").addClass("invisible").removeClass("visible");
+});
+
+$(".button_su_inner").mouseenter(function (e) {
+  var parentOffset = $(this).offset();
+
+  var relX = e.pageX - parentOffset.left;
+  var relY = e.pageY - parentOffset.top;
+  $(this).prev(".su_button_circle").css({
+    "left": relX,
+    "top": relY
   });
-  $(".avatar2").hover(function () { //"test_enter"요소에 '마우스가진입'하면
-    $(".bubble2").addClass("visible").removeClass("invisible");
-    $(".bubble1, .bubble3, .bubble4, .bubble5").addClass("invisible").removeClass("visible");
+  $(this).prev(".su_button_circle").removeClass("desplode-circle");
+  $(this).prev(".su_button_circle").addClass("explode-circle");
+
+});
+
+$(".button_su_inner").mouseleave(function (e) {
+
+  var parentOffset = $(this).offset();
+
+  var relX = e.pageX - parentOffset.left;
+  var relY = e.pageY - parentOffset.top;
+  $(this).prev(".su_button_circle").css({
+    "left": relX,
+    "top": relY
   });
-  $(".avatar3").hover(function () { //"test_enter"요소에 '마우스가진입'하면
-    $(".bubble3").addClass("visible").removeClass("invisible");
-    $(".bubble1, .bubble2, .bubble4, .bubble5").addClass("invisible").removeClass("visible");
-  });
-  $(".avatar4").hover(function () { //"test_enter"요소에 '마우스가진입'하면
-    $(".bubble4").addClass("visible").removeClass("invisible");
-    $(".bubble1, .bubble2, .bubble3, .bubble5").addClass("invisible").removeClass("visible");
-  });
-  $(".avatar5").hover(function () { //"test_enter"요소에 '마우스가진입'하면
-    $(".bubble5").addClass("visible").removeClass("invisible");
-    $(".bubble1, .bubble2, .bubble3, .bubble4").addClass("invisible").removeClass("visible");
-  });
+  $(this).prev(".su_button_circle").removeClass("explode-circle");
+  $(this).prev(".su_button_circle").addClass("desplode-circle");
+
+});
